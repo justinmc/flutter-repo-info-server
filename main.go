4ab577c767dde2cd3ca8862bf6dba4b81f6cacf3
Justin McCandless <justinjmccandless@gmail.com>
2022-01-13 14:21:07 -0800

    Basic server

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "log"
+  "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
 
 func main() {
-	fmt.Println("Hello, world.")
+  fmt.Println("Starting server...")
+  http.HandleFunc("/", handler)
+  log.Fatal(http.ListenAndServe(":8080", nil))
 }

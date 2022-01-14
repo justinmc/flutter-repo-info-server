54d7cc54149aa5542a0561e9fed4608518cccd74
Justin McCandless <justinjmccandless@gmail.com>
2022-01-13 22:03:16 -0800

    Can check if a SHA is in stable

--- api/github.go
+++ api/github.go
@@ -9,16 +9,58 @@ import (
   "strconv"
 )
 
-const kAPI = "https://api.github.com/repos/flutter/flutter";
+const kAPI string = "https://api.github.com/repos/flutter/flutter";
+const kBranch string = "master";
+
+// TODO(justinmc): This should be scraped from https://docs.flutter.dev/development/tools/sdk/releases.
+const kLatestStable string = "77d935a";
+const kLatestBeta string = "628f0e3";
+
 
 type PR struct {
   MergeCommitSha string `json:"merge_commit_sha"`
   MergedAt string `json:"merged_at"`
+  Base Branch `json:"base"`
+}
+
+type Branch struct {
+  Ref string `json:"ref"`
+}
+
+type Comparison struct {
+  Status string `json:status`
+}
+
+// Returns true iff the given sha is on the current stable channel.
+//
+// This doesn't consider reverts.
+func IsInStable(sha string) (bool, error) {
+  //  http get https://api.github.com/repos/flutter/flutter/compare/77d935a...f3947ea
+  resp, err := http.Get(kAPI + "/compare/" + kLatestStable + "..." + sha);
+  if (err != nil) {
+    return false, err;
+  }
+
+  body, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    return false, err;
+  }
+  bodyString := string(body)
+
+  var comparison Comparison;
+  json.Unmarshal([]byte(bodyString), &comparison)
+
+  if (comparison.Status == "") {
+    return false, errors.New("Unable to find the given SHA.");
+  }
+
+  return comparison.Status == "behind" || comparison.Status == "identical", nil;
 }
 
 // Returns the SHA for the merge commit of the given PR.
 //
-// Returns an error if the PR doesn't exist or isn't merged.
+// Returns an error if the PR doesn't exist, isn't merged, or isn't based on
+// master.
 func GetPrMergeCommit(prNumber int) (string, error) {
   resp, err := http.Get(kAPI + "/pulls/" + strconv.Itoa(prNumber));
   if (err != nil) {
@@ -39,6 +81,9 @@ func GetPrMergeCommit(prNumber int) (string, error) {
   }
   if (pr.MergedAt == "") {
     return "", errors.New("PR not yet merged.");
+  }
+  if (pr.Base.Ref != kBranch) {
+    return "", errors.New("PR's base isn't master");
   }
 
   return pr.MergeCommitSha, nil;
--- main.go
+++ main.go
@@ -18,12 +18,21 @@ func main() {
   //var merged bool = prWasMerged(95948); // Merged.
   //fmt.Println("justin merged?", merged);
 
+  /*
   mergeCommit, err := api.GetPrMergeCommit(95948);
   if (err != nil) {
     log.Fatalln(err);
   }
 
   fmt.Println("justin merge commit", mergeCommit);
+  */
+
+  isInStable, err := api.IsInStable("f3947ea"); // In stable.
+  //isInStable, err := api.IsInStable("8b46014"); // Not in stable (yet).
+  if (err != nil) {
+    log.Fatalln(err);
+  }
+  fmt.Println("justin is in stable?", isInStable);
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
